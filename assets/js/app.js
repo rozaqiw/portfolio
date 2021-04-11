003332e5a98518cb8f1e1112d232c6cff8ea9c66
Abdul Rozaqi W <61746302+rozaqiw@users.noreply.github.com>
2021-04-11 12:18:12 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,7 +17,7 @@ let header = $(`
   <ul class="navbar-nav ml-auto" id = "navbar-content">
    <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
    <li class="nav-item"><a class="nav-link" href="experience.html">Experience</a></li>
-   <li class="nav-item"><a class="nav-link" href="projects.html">Projects</a></li>
+   <li class="nav-item"><a class="nav-link" href="profile.html">Profile</a></li>
    <li class="nav-item"><a class="nav-link" href="research.html">Blog</a></li>
    <li class="nav-item"><a class="nav-link" href="education.html">Education</a></li>
   </ul>
